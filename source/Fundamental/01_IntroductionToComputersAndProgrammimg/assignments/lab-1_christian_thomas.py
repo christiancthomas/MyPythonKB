666c359e7429db469b3332ce5188cd624a3f696c
Christian Thomas <76794573+christiancthomas@users.noreply.github.com>
2025-06-10 18:09:59 -0500

    old lab1 assignment

---
+++ source/Fundamental/01_IntroductionToComputersAndProgrammimg/assignments/lab-1_christian_thomas.py
@@ -0,0 +1,18 @@
+"""
+Write three Python statements to display (print) your name, college major and favorite movie. (See Section 2.3 on using the print function).
+Save the file: Click File>Save As> yourlastname_Lab1.py
+Run the program in IDLE: Run>Run Module. Correct any errors and rerun.
+To turn in: Select LAB 1 Orientation above. Scroll down until you see Attach File: Browse My Computer. 
+Click on this and attach the python file you just created. Scroll down more and press Submit.
+"""
+
+print('Name: Christian Thomas')
+print('Major: Mechanical Engineering')
+print('Favorite Movie: When Harry Met Sally')
+
+
+print(format(0.2, '.0%'))
+x = int(68.749)
+print('x =', x)
+print(2.0 * 12)
+print('The path is D:\\sample\\test.')
